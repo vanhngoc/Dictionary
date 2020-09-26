8b6a2bca3ea1416bf6320e1ddc4eb054c9ac177e
vanhngoc <vanhngoc1001@gmail.com>
2020-09-26 23:51:06 +0700

    fix

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -3,12 +3,11 @@ import java.util.ArrayList;
 public class DictionaryCommandline {
 
     public void showAllWords(ArrayList<Word> wordList) {
-        System.out.print("N0\t" + "|English\t" + "| " + "Vietnamese" + "\n");
+        System.out.print("N0\t" + "|English\t\t" + "|" + "Vietnamese" + "\n");
         for (int i = 0; i < wordList.size(); i++) {
             System.out.print(i + 1);
-            System.out.print("\t|" + wordList.get(i).getWord_target() + "\t");
-            System.out.print("\t" + wordList.get(i).getWord_explain() + "\n");
-
+            System.out.print("\t|" + wordList.get(i).getWord_target() + "\t\t");
+            System.out.print("\t|" + wordList.get(i).getWord_explain() + "\n");
         }
     }
 }
--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -8,13 +8,14 @@ public class DictionaryManagement {
         Scanner scanner = new Scanner(System.in);
         Dictionary dictionary = new Dictionary();
         int numberWords = scanner.nextInt();
+        scanner.nextLine();
         while (0 != numberWords) {
             Word word = new Word();
             System.out.println("Nhap tu tieng anh : ");
-            word.setWord_target(scanner.next());
+            word.setWord_target(scanner.nextLine());
 
             System.out.println("Nhap giai thich sang tieng viet");
-            word.setWord_explain(scanner.next());
+            word.setWord_explain(scanner.nextLine());
             dictionary.wordList.add(word);
             numberWords--;
         }
